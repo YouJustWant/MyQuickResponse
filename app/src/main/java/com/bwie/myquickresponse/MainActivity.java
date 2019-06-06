0d61924ce7d4e83e6557a7918c934e0a01d0173a
YouJustWant <1152722231@qq.com>
2019-06-06 15:59:56 +0800

    权限修改，功能增加

--- app/src/main/java/com/bwie/myquickresponse/MainActivity.java
+++ app/src/main/java/com/bwie/myquickresponse/MainActivity.java
@@ -1,21 +1,54 @@
 package com.bwie.myquickresponse;
 
+import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.uuzuche.lib_zxing.activity.CaptureActivity;
 import com.uuzuche.lib_zxing.activity.CodeUtils;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        request();
+        //
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+        builder.detectFileUriExposure();
+    }
+
+    /**
+     *
+     *
+     */
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    void request() { //获取相机拍摄读写权限
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { //版本判断
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1);
+            }
+        }
     }
 
     /***
@@ -27,6 +60,24 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, 0);
     }
 
+    /***
+     *
+     * @param view
+     */
+    public void choose(View view) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        startActivityForResult(intent, 1);
+    }
+    /***
+     *
+     * @param view
+     */
+    public void quick(View view) {
+        Intent intent = new Intent(MainActivity.this,CreateActivity.class);
+        startActivity(intent);
+    }
     /**
      * @param requestCode
      * @param resultCode
@@ -46,6 +97,31 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
                 } else if (bundle.getInt(CodeUtils.RESULT_TYPE) == CodeUtils.RESULT_FAILED) {
                     Toast.makeText(this, "解析二维码失败", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+        if (requestCode == 1) {
+            if (null != data) {
+                Uri uri = data.getData();
+                ContentResolver resolver = getContentResolver();
+                try {
+                    Bitmap mbitmap = MediaStore.Images.Media.getBitmap(resolver, uri);
+                    CodeUtils.analyzeBitmap(ImageUtils.getImageAbsolutePath(this,uri), new CodeUtils.AnalyzeCallback() {
+                        @Override
+                        public void onAnalyzeSuccess(Bitmap mBitmap, String result) {
+                            Toast.makeText(MainActivity.this, "解析结果：" + result, Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onAnalyzeFailed() {
+                            Toast.makeText(MainActivity.this, "解析二维码失败", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    if (mbitmap != null) {
+                        mbitmap.recycle();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         }
